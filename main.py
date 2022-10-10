eb574f03a3d30ca211b8afad09eaf8ebbe7df610
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-10-10 13:10:38 +0200

    Finished song queue implementation. Added youtube search by name.

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ for i in range(len(cogs)):
 
 @client.event
 async def on_ready():
-    print('Siema. Jestem {0.user}'.format(client))
+    print('Siemafsad. Jestem {0.user}'.format(client))
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is not None and len(before.channel.members) == 1 and before.channel.members[0].bot:
+        try:
+            await before.channel.members[0].move_to(None)
+        except:
+            pass    
 
 
-with open("token") as file:
+with open(R"C:\Users\mateu\dc_bot\token") as file:
     client.run(file.readline())

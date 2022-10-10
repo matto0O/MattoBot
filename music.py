1879d9d28e870d21c44d2b68d79c439ce5c95538
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-10-10 13:12:05 +0200

    Finished song queue implementation. Added youtube search by name.

--- music.py
+++ music.py
@@ -1,11 +1,8 @@
-import queue
-import time
-
 import discord
+import asyncio
 from discord.ext import commands
 import youtube_dl
-from threading import Thread
-import threading
+from youtube_search import YoutubeSearch
 
 
 class SoundQueue:
@@ -32,24 +29,24 @@ class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.queue = SoundQueue()
-        self.callback_queue = queue.Queue()
 
     @commands.command()
     async def join(self, ctx):
         if ctx.author.voice is None:
-            await ctx.send("Bruv... join a channel!")
+            await ctx.send("Gdzie jest głośnik?!")
         voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
+            self.queue.empty()
         elif ctx.voice_client.is_playing():
-            await ctx.send("Sorry, I'm busy!")
+            await ctx.send("Z A M K N I J  R Y J")
         else:
             await ctx.voice_client.move_to(voice_channel)
 
     @commands.command()
     async def stop(self, ctx):
         if ctx.author.voice.channel is not ctx.voice_client.channel or not ctx.voice_client.is_playing:
-            await ctx.send("Nothing to stop")
+            await ctx.send("Tam nikogo nie ma")
         else:
             ctx.voice_client.stop()
 
@@ -59,42 +56,34 @@ class Music(commands.Cog):
             self.queue.empty()
             await ctx.voice_client.disconnect()
         else:
-            await ctx.send("You can't disconnect the bot while not on its channel!")
+            await ctx.send("Z A M K N I J  R Y J")
 
-    async def play_dc(self, ctx):
-        if self.queue.size() > 0:
-            await self.play(ctx, self.queue.dequeue())
-        else:
-            await self.dc(ctx)
+    def run(self, ctx, src):
+        ctx.voice_client.play(src, after=(lambda x=None: self.check_for_next(ctx)))      
 
     def check_for_next(self, ctx):
-        while ctx.voice_client.is_playing():
-            pass
-        self.callback_queue.put(self.play_dc)
+        if self.queue.size() > 0:
+            asyncio.run(self.play(ctx, self.queue.dequeue()))
 
     @commands.command()
-    async def play(self, ctx, url):
+    async def play(self, ctx, url, *args):
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 3',
                           'options': '-vn'}
-        YDL_OPTIONS = {'format': "bestaudio"}
+        YDL_OPTIONS = {'format': "bestaudio", 'noplaylist':'True'}
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-            await self.join(ctx)
-            vc = ctx.voice_client
-            vc.play(source)
-            # player = Thread(target=vc.play, args=(source,))
-            # player.start()
-            # checker = Thread(target=self.check_for_next, args=(ctx,))
-            # checker.start()
-            # while True:
-            #     try:
-            #         callback = self.callback_queue.get(False)
-            #         break
-            #     except queue.Empty:
-            #         print("check")
-            # await callback(ctx)
+            try:
+                info = ydl.extract_info(url, download=False)  
+            except youtube_dl.utils.DownloadError:
+                if len(args) == 0:
+                    args = []  
+                new_url = "https://www.youtube.com{}".format(YoutubeSearch(url + ' '.join(args), max_results=1).to_dict()[0]["url_suffix"])
+                info = ydl.extract_info(new_url, download=False)     
+            finally:
+                await ctx.send("Teraz leci: \n{}".format(info["title"]))
+                url2 = info['formats'][0]['url']
+                source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+                await self.join(ctx)
+                self.run(ctx, source)
 
     @commands.command()
     async def add(self, ctx, url):
@@ -110,14 +99,13 @@ class Music(commands.Cog):
             "https://www.youtube.com/watch?v=Sug433bP-mw", # impreza
             "https://www.youtube.com/watch?v=xm_ujA1CXCc", # testarossa
             "https://www.youtube.com/watch?v=iewMEY-66yw", # kokaina
-            "https://youtu.be/3dHpEfmegOA" # harnaś ice tea
+            "https://youtu.be/3dHpEfmegOA", # harnaś ice tea
+            "https://youtu.be/i92EzcnOMJY" # bandyta
         ]
         comms = [
-            "Gdzie jest głośnik?!",
             "Ale mam esse :cowboy: :call_me: ",
-            "Tam nikogo nie ma",
             "Ktoś zajebał misclicka",
-            "Z A M K N I J  R Y J"
+            "Ogarniemy stary :sunglasses: ",
         ]
         await ctx.send(random.choice(comms))
         await self.play(ctx, random.choice(songs))
